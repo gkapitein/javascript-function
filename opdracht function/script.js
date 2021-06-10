5a8ad4cc88eb970cd3116ee070bc2c4f9a1386ff
gkapitein <84490555+gkapitein@users.noreply.github.com>
2021-06-10 08:34:05 +0200

    Add files via upload

---
+++ opdracht function/script.js
@@ -0,0 +1,13 @@
+const makePizza = function () {
+    console.log("maak deeg");
+    console.log("laat het deeg rusten");
+    console.log("snijd ondertussen de groente en het vlees");
+    console.log("rol je deeg uit en bestrijk met groenten en vlees");
+    console.log("bak de over in 30 mintuten af op 130 graden");
+}
+
+makePizza();
+
+makePizza();
+
+makePizza();
